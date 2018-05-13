b747e42f9a8b7a4d526012ee61b5e8b09cee3b25
yosuke ota <otameshiyo23@gmail.com>
2018-05-13 21:21:54 +0900

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+"use strict"
+
+
+module.exports = {
+  parserOptions: {
+      ecmaVersion: 2018,
+  },
+  extends: [
+    // add more generic rulesets here, such as:
+    'eslint:all',
+    'plugin:lodash-template/recommended'
+  ],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'lodash-template/no-warning-html-comments': 'error'
+    'multiline-ternary': ["error", 'always-multiline']
+  }
+}
